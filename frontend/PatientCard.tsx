8468d7a46cc3c3299b589a15716242892c5518e4
sonypailla <sonypailla7@gmail.com>
2026-06-13 10:04:27 -0500

    Add PatientCard component

---
+++ frontend/PatientCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function PatientCard() {
+  return (
+    <div>
+      <h2>John Doe</h2>
+      <p>DOB: 1990-01-01</p>
+      <p>Gender: Male</p>
+    </div>
+  );
+}
